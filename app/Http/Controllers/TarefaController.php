e7d7aa2367f1de003b2a80561479788e2f7d44aa
Maryjanesilva <ribeirodasilvam083@gmail.com>
2025-03-20 09:22:47 -0300

    adicionado model migration tarefa

--- app/Http/Controllers/TarefaController.php
+++ app/Http/Controllers/TarefaController.php
@@ -5,6 +5,28 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class TarefaController extends Controller
+
 {
-    //
+
+    public function index(){
+
+    }
+    public function store(Request $request){
+
+    }
+
+
+    public function show(string $id){
+
+    }
+
+
+
+    public function update (Request $request,string $id){
+
+    }
+
+    public function destroy (string $id){
+        
+    }
 }
